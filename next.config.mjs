ebb2e9374d3ccedd46b123af91e542b30a14daf3
Nguyễn Đức Hoài <nguyenduchoai@gmail.com>
2025-08-16 23:37:55 +0700

    rroot

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true, // Tắt tối ưu hóa hình ảnh của Next.js
+  },
+};
+
+export default nextConfig;
+
+
